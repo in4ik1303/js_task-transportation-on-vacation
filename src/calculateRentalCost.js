4ec0aebba010ee1b8393a3b15d2275a3018bdeac
Inna Nedohybchenko <inna_roeva@ukr.net>
2025-09-25 16:56:25 +0300

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  if (typeof days !== 'number' || days <= 0) {
+    return 0;
+  }
+
+  let sum = days * 40;
+
+  if (days >= 7) {
+    sum -= 50;
+  } else if (days >= 3) {
+    sum -= 20;
+  }
+
+  return sum;
 }
 
 module.exports = calculateRentalCost;
