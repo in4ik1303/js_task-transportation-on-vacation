d568178a083f01a5c23b98fb7421783f5b788213
Inna Nedohybchenko <inna_roeva@ukr.net>
2025-09-26 17:28:26 +0300

    made improvements 2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -9,14 +9,15 @@ function calculateRentalCost(days) {
   const MEDIUM_TERM_DAYS = 3;
   const LONG_TERM_DISCOUNT = 50;
   const MEDIUM_TERM_DISCOUNT = 20;
+  const numberOfDays = days;
 
-  const baseCost = days * DAILY_RATE;
+  const baseCost = numberOfDays * DAILY_RATE;
 
-  if (days >= LONG_TERM_DAYS) {
+  if (numberOfDays >= LONG_TERM_DAYS) {
     return baseCost - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= MEDIUM_TERM_DAYS) {
+  if (numberOfDays >= MEDIUM_TERM_DAYS) {
     return baseCost - MEDIUM_TERM_DISCOUNT;
   }
 
